49f5e97e618334ed005d2358f29d3f7619ad0085
Nicolas <nicolasgraas@hotmail.com>
2024-05-06 22:22:54 +0200

    Added env variables

--- files/start.sh
+++ files/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-s=/mnt/foundry/server
-p=/mnt/foundry/persistentdata
-echo "Setting timezone to $TZ"
+# Location of server data and save data for docker
+
+server_files=/mnt/foundry/server
+persistent_data=/mnt/foundry/persistentdata
+
+echo "Setting time zone to $TZ"
 echo $TZ > /etc/timezone 2>&1
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 2>&1
 dpkg-reconfigure -f noninteractive tzdata 2>&1
@@ -11,15 +14,24 @@ chmod -R 777 /root/.steam 2>/dev/null
 echo " "
 echo "Updating Foundry Dedicated Server files..."
 echo " "
-/usr/bin/steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$s" +login anonymous +app_update 2915550 validate +quit
-echo "steam_appid: "`cat $s/steam_appid.txt`
+/usr/bin/steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir "$server_files" +login anonymous +app_update 2915550 validate +quit
+echo "steam_appid: "`cat $server_files/steam_appid.txt`
 echo " "
-echo "Checking if app.cfg files exists"
-if [ ! -f "$s/app.cfg" ]; then
-        echo "$s/app.cfg not found. Copying default file."
-        cp "/home/steam/app.cfg" "$s/" 2>&1
+echo "Checking if app.cfg files exists and no env virables were set"
+if [ ! -f "$server_files/app.cfg" ]; then
+    echo "$server_files/app.cfg not found. Copying default file."
+    cp "/home/steam/app.cfg" "$server_files/" 2>&1
 fi
-cd "$s"
+
+echo "Checking if CUSTOM_CONFIG env is set:"
+if [ ! -z $CUSTOM_CONFIG ]; then
+    echo "Not changing app.cfg file"
+else
+    echo "Running setup script for the app.cfg file"
+    source ./env2cfg.sh
+fi
+
+cd "$server_files"
 echo "Starting Foundry Dedicated Server"
 echo " "
 echo "Starting Xvfb"
