1d7e7d324d6229d31c7346298952732537e1aa6f
luxusburg <git@luxusburg.lu>
2024-05-11 02:09:59 +0200

    Update start.sh for non-root

--- files/start.sh
+++ files/start.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 # Location of server data and save data for docker
 
-server_files=/mnt/foundry/server
-persistent_data=/mnt/foundry/persistentdata
+server_files=/app/serverfiles
+persistent_data=/app/data
 
-echo "Setting time zone to $TZ"
-echo $TZ > /etc/timezone 2>&1
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime 2>&1
-dpkg-reconfigure -f noninteractive tzdata 2>&1
-cron
-
-mkdir -p /root/.steam 2>/dev/null
-chmod -R 777 /root/.steam 2>/dev/null
+mkdir -p /home/steam/.steam/ 2>/dev/null
+chmod -R 777 /home/steam/.steam 2>/dev/null
 echo " "
 echo "Updating Foundry Dedicated Server files..."
 echo " "
@@ -27,9 +21,9 @@ else
     # For the docker image save files to work, the persistent data folder can not be changed in app.cfg
     echo "Setting persistent data folder in app.cfg for the image to work correctly"
     if grep -q "server_persistent_data_override_folder" $server_files/app.cfg; then
-        sed -i "/server_persistent_data_override_folder=/c server_persistent_data_override_folder=/mnt/foundry/persistentdata" $server_files/app.cfg
+        sed -i "/server_persistent_data_override_folder=/c server_persistent_data_override_folder=/app/data" $server_files/app.cfg
     else
-        echo -ne '\nserver_persistent_data_override_folder=/mnt/foundry/persistentdata' >> $server_files/app.cfg
+        echo -ne '\nserver_persistent_data_override_folder=/app/data' >> $server_files/app.cfg
     fi
 fi
 echo " "
@@ -37,11 +31,11 @@ echo " "
 echo "Checking if CUSTOM_CONFIG env is set and if set to true:"
 if [ ! -z $CUSTOM_CONFIG ]; then
     if [ $CUSTOM_CONFIG = true ];then
-	    echo "Not changing app.cfg file"
-	else
-	    echo "Running setup script for the app.cfg file"
+            echo "Not changing app.cfg file"
+        else
+            echo "Running setup script for the app.cfg file"
             source ./env2cfg.sh
-	fi
+        fi
     
 else
     echo "Running setup script for the app.cfg file"
@@ -73,17 +67,18 @@ rm /tmp/.X0-lock
 rm -r /tmp/*
 
 cd "$server_files"
+
 echo "Starting Foundry Dedicated Server"
 echo " "
 echo "Starting Xvfb"
-Xvfb :0 -screen 0 640x480x24:32 &
+Xvfb :0 -screen 0 640x480x24:32 -nolisten tcp -nolisten unix &
 
 # save PID of Xvfb process for clean shutdown
 XVFB_PID=$!
 
 echo "Launching wine Foundry"
 echo " "
-DISPLAY=:0.0 wine /mnt/foundry/server/FoundryDedicatedServer.exe -log 2>&1
+DISPLAY=:0.0 wine $server_files/FoundryDedicatedServer.exe -log 2>&1
 
 # make sure Xvfb process will be stopped and remove lock
 kill $XVFB_PID
