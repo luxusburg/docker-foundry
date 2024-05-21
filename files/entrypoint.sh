a6034d1d2237b9dd25e6f9ee8a059c6ecd544d24
luxusburg <git@luxusburg.lu>
2024-05-21 12:36:28 +0200

    Removed unused lines

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,6 +3,3 @@
 # To be able to use the volumes user foundry needs access
 doas -u root chown -R foundry:foundry /home/foundry
 exec "$@"
-#chown -R foundry:foundry /tmp
-#chown -R foundry:foundry /var/spool/cron/crontabs/root
-#exec runuser -u foundry "$@"
